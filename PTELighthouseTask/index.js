76b57ebe0cf35c2da140b34eb1da930f37916c6d
Sairam2112002 <sairam2112002@gmail.com>
2024-04-06 17:42:36 +0530

    PTELighthouseTask completed

--- PTELighthouseTask/index.js
+++ PTELighthouseTask/index.js
@@ -11,7 +11,26 @@ const puppeteer = require('puppeteer');
     const pteApplicationUri = "http://localhost/";
 
     // Locators
-    const tablesSection = "li.page-item-13 > a";
+    const tablesSectionButton = "li.page-item-13 > a";
+    const livingRoomTable6 = "div.product-117 h3";
+    const addToCartButton = "#primary button";
+    const cartSectionButton = "li.page-item-31 > a";
+    const placeAnOrderButton = "div.form-buttons > input";
+    const companyInputField = "form > div:nth-of-type(2) > div:nth-of-type(2) input";
+    const fullNameInputField = "div:nth-of-type(2) > div:nth-of-type(3) input";
+    const addressInputField = "div:nth-of-type(2) > div:nth-of-type(4) input";
+    const postalCodeInputField = "div:nth-of-type(2) > div:nth-of-type(5) input";
+    const cityInputField = "div:nth-of-type(2) > div:nth-of-type(6) input";
+    const countryDropdown = "div:nth-of-type(2) > div.dropdown_country select";
+    const stateDropdown = "div:nth-of-type(2) > div.dropdown_state select";
+    const phoneInputField = "div:nth-of-type(2) > div:nth-of-type(9) input";
+    const emailInputField = "div:nth-of-type(2) > div.email input";
+    const commentsTextAreaField = "form > div:nth-of-type(2) textarea";
+    const finalPlaceOrderButton = "div.cart_button > input";
+    
+    // Constants
+    const countryToSelect = "IN";
+    const stateToSelect = "IN_AP";
 
     const lhApi = await import('lighthouse');
     const flags = {
@@ -48,373 +67,80 @@ const puppeteer = require('puppeteer');
     { 
         await Promise.all([
             page.waitForNavigation(),
-            page.click(tablesSection)
-          ]);
+            page.click(tablesSectionButton)
+        ]);
     }
     await lhFlow.endNavigation();
     
     // Select a table
     await lhFlow.startNavigation();
     {
-        const targetPage = page;
-        const promises = [];
-        const startWaitingForEvents = () => {
-            promises.push(targetPage.waitForNavigation());
-        }
-        await puppeteer.Locator.race([
-            targetPage.locator('div.product-117 h3'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"post-13\\"]/div/div[2]/div[3]/a/h3)')
-        ])
-            .setTimeout(timeout)
-            .on('action', () => startWaitingForEvents())
-            .click({
-              offset: {
-                x: 93.921875,
-                y: 23.25,
-              },
-            });
-        await Promise.all(promises);
+        await Promise.all([
+            page.waitForNavigation(),
+            page.click(livingRoomTable6)
+        ]);
     }
     await lhFlow.endNavigation();
 
     // Add table to Cart
     await lhFlow.startTimespan();
     {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('#primary button'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"reg_add_117\\"]/button)')
-        ])
-            .setTimeout(timeout)
-            .click({
-              offset: {
-                x: 82.015625,
-                y: 22.703125,
-              },
-            });
+        await Promise.all([page.click(addToCartButton)]);
     }
     await lhFlow.endTimespan();
 
     // Navigate to Cart Page
     await lhFlow.startNavigation();
     {
-        const targetPage = page;
-        const promises = [];
-        const startWaitingForEvents = () => {
-            promises.push(targetPage.waitForNavigation());
-        }
-        await puppeteer.Locator.race([
-            targetPage.locator('li.page-item-31 > a'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"masthead\\"]/div[1]/div/div[2]/div/ul/li[6]/a)')
-        ])
-            .setTimeout(timeout)
-            .on('action', () => startWaitingForEvents())
-            .click({
-              offset: {
-                x: 18.109375,
-                y: 15.453125,
-              },
-            });
-        await Promise.all(promises);
+        await Promise.all([
+            page.waitForNavigation(),
+            page.click(cartSectionButton)
+        ]);
     }
     await lhFlow.endNavigation();
 
     // Checkout
     await lhFlow.startNavigation();
     {
-        const targetPage = page;
-        const promises = [];
-        const startWaitingForEvents = () => {
-            promises.push(targetPage.waitForNavigation());
-        }
-        await puppeteer.Locator.race([
-            targetPage.locator('div.form-buttons > input'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-container\\"]/form/div[2]/input)')
-        ])
-            .setTimeout(timeout)
-            .on('action', () => startWaitingForEvents())
-            .click({
-              offset: {
-                x: 70.40625,
-                y: 23.21875,
-              },
-            });
-        await Promise.all(promises);
+        await Promise.all([
+            page.waitForNavigation(),
+            page.click(placeAnOrderButton)
+        ]);
     }
     await lhFlow.endNavigation();
 
-    // Enter required details and purchase
+    // Enter required details
     await lhFlow.startTimespan();
     {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('form > div:nth-of-type(2) > div:nth-of-type(2) input'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[2]/div[2]/span/input)')
-        ])
-            .setTimeout(timeout)
-            .click({
-              offset: {
-                x: 241,
-                y: 28.6875,
-              },
-            });
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('form > div:nth-of-type(2) > div:nth-of-type(2) input'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[2]/div[2]/span/input)')
-        ])
-            .setTimeout(timeout)
-            .fill('Gryffindor');
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('div:nth-of-type(2) > div:nth-of-type(3) input'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[3]/div[2]/span/input)')
-        ])
-            .setTimeout(timeout)
-            .click({
-              offset: {
-                x: 54,
-                y: 16.703125,
-              },
-            });
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('div:nth-of-type(2) > div:nth-of-type(3) input'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[3]/div[2]/span/input)')
-        ])
-            .setTimeout(timeout)
-            .fill('H');
-    }
-    {
-        const targetPage = page;
-        await targetPage.keyboard.up('h');
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('div:nth-of-type(2) > div:nth-of-type(3) input'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[3]/div[2]/span/input)')
-        ])
-            .setTimeout(timeout)
-            .fill('Harry Potter');
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('div:nth-of-type(2) > div:nth-of-type(4) input'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[4]/div[2]/span/input)')
-        ])
-            .setTimeout(timeout)
-            .click({
-              offset: {
-                x: 67,
-                y: 12.703125,
-              },
-            });
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('div:nth-of-type(2) > div:nth-of-type(4) input'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[4]/div[2]/span/input)')
-        ])
-            .setTimeout(timeout)
-            .fill('Hogwarts');
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('div:nth-of-type(2) > div:nth-of-type(5) input'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[5]/div[2]/span/input)')
-        ])
-            .setTimeout(timeout)
-            .click({
-              offset: {
-                x: 19,
-                y: 12.703125,
-              },
-            });
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('div:nth-of-type(2) > div:nth-of-type(5) input'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[5]/div[2]/span/input)')
-        ])
-            .setTimeout(timeout)
-            .fill('P44');
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('div:nth-of-type(2) > div:nth-of-type(6) input'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[6]/div[2]/span/input)')
-        ])
-            .setTimeout(timeout)
-            .click({
-              offset: {
-                x: 38,
-                y: 13.703125,
-              },
-            });
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('div:nth-of-type(2) > div:nth-of-type(6) input'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[6]/div[2]/span/input)')
-        ])
-            .setTimeout(timeout)
-            .fill('Hogsmeade');
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('div:nth-of-type(2) > div.dropdown_country select'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[7]/div[2]/select)')
-        ])
-            .setTimeout(timeout)
-            .click({
-              offset: {
-                x: 159,
-                y: 9.703125,
-              },
-            });
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('div:nth-of-type(2) > div.dropdown_country select'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[7]/div[2]/select)')
-        ])
-            .setTimeout(timeout)
-            .fill('IN');
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('div:nth-of-type(2) > div.dropdown_state select'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[8]/div[2]/select)')
-        ])
-            .setTimeout(timeout)
-            .click({
-              offset: {
-                x: 125,
-                y: 10.703125,
-              },
-            });
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('div:nth-of-type(2) > div.dropdown_state select'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[8]/div[2]/select)')
-        ])
-            .setTimeout(timeout)
-            .fill('IN_AP');
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('div:nth-of-type(2) > div:nth-of-type(9) input'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[9]/div[2]/span/input)')
-        ])
-            .setTimeout(timeout)
-            .click({
-              offset: {
-                x: 310,
-                y: 16.703125,
-              },
-            });
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('div:nth-of-type(2) > div:nth-of-type(9) input'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[9]/div[2]/span/input)')
-        ])
-            .setTimeout(timeout)
-            .fill('12345678');
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('div:nth-of-type(2) > div.email input'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[10]/div[2]/span/input)')
-        ])
-            .setTimeout(timeout)
-            .click({
-              offset: {
-                x: 154,
-                y: 30.703125,
-              },
-            });
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('div:nth-of-type(2) > div.email input'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[10]/div[2]/span/input)')
-        ])
-            .setTimeout(timeout)
-            .fill('hp@gmail.com');
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('form > div:nth-of-type(2) textarea'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[11]/div[2]/textarea)')
-        ])
-            .setTimeout(timeout)
-            .click({
-              offset: {
-                x: 133,
-                y: 11.71875,
-              },
-            });
-    }
-    {
-        const targetPage = page;
-        await puppeteer.Locator.race([
-            targetPage.locator('form > div:nth-of-type(2) textarea'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[2]/div[11]/div[2]/textarea)')
-        ])
-            .setTimeout(timeout)
-            .fill('nope.');
+        await page.type(companyInputField, "Gryffindor");
+        await page.type(fullNameInputField, "Harry Potter");
+        await page.type(addressInputField, "Hogwarts");
+        await page.type(postalCodeInputField, "P44");
+        await page.type(cityInputField, "Hogsmeade");
+        await page.select(countryDropdown, countryToSelect);
+
+        await page.waitForSelector(stateDropdown);
+        await page.select(stateDropdown, stateToSelect);
+
+        await page.type(phoneInputField, "1234567890");
+        await page.type(emailInputField, "hp@gmail.com");
+        await page.type(commentsTextAreaField, "No comments");
     }
     await lhFlow.endTimespan();
 
+    // Purchase
     await lhFlow.startNavigation();
     {
-        const targetPage = page;
-        const promises = [];
-        const startWaitingForEvents = () => {
-            promises.push(targetPage.waitForNavigation());
-        }
-        await puppeteer.Locator.race([
-            targetPage.locator('div.cart_button > input'),
-            targetPage.locator('::-p-xpath(//*[@id=\\"shopping-cart-submit-container\\"]/div/div/div/form/div[4]/input)')
-        ])
-            .setTimeout(timeout)
-            .on('action', () => startWaitingForEvents())
-            .click({
-              offset: {
-                x: 123.234375,
-                y: 25.671875,
-              },
-            });
-        await Promise.all(promises);
+        await Promise.all([
+            page.waitForNavigation,
+            page.click(finalPlaceOrderButton)
+        ]);
     }
     await lhFlow.endNavigation();
 
-    const lhFlowReport = await lhFlow.generateReport();
-    fs.writeFileSync(__dirname + '/flow.report.html', lhFlowReport)
+    // Generate Lighthouse Report
+    const lighthouseReport = await lhFlow.generateReport();
+    fs.writeFileSync(__dirname + '/lighthouseReport.html', lighthouseReport)
 
     await browser.close();
 
